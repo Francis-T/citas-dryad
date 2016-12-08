32f91a81c9ab2ad5a5be3b9e41dde99686770dcf
jerelynco <jerelynco@gmail.com>
2016-12-08 13:58:03 +0800

    Saving edits

--- dryad_mt/request_handler.py
+++ dryad_mt/request_handler.py
@@ -1,7 +1,7 @@
 """
-    Name: request_handler.py
-    Author: Francis T
-    Desc: Source code for the Cache Node request handler
+	Name: request_handler.py
+	Author: Francis T
+	Desc: Source code for the Cache Node request handler
 """
 import logging
 import json
@@ -13,170 +13,178 @@ from dryad.database import DryadDatabase
 from dryad_mt.node_state import NodeState
 
 class RequestHandler():
-    def __init__(self, event, queue, state, db_name):
-        self.hevent = event
-        self.queue = queue
-        self.nstate = state
-        self.dbname = db_name
-        
-        self.logger = logging.getLogger("main.RequestHandler")
-        return
+	def __init__(self, event, queue, state, db_name):
+		self.hevent = event
+		self.queue = queue
+		self.nstate = state
+		self.dbname = db_name
+		self.logger = logging.getLogger("main.RequestHandler")
+		return
+	
+	def handle_req_state(self, link, content):
+		#state = self.nstate.get_state()
+		#TODO Retrieve information from database
+		state = "{'state':'inactive','batt':'100','version':'1.0','lat':'10.12','lon':'122.11'}"
+		return link.send_response("RSTAT:" + state + ";\r\n")
+	
+	def handle_req_activate(self, link, content):
+		# Add an activation request to the main cache node queue
+		self.queue.put("ACTIVATE")
+		# And trigger the event handler
+		self.hevent.set()
+		return link.send_response("RACTV:OK;\r\n")
 
-    def handle_req_state(self, link, content):
-        state = self.nstate.get_state()
-        return link.send_response("RSTAT:" + state + ";\r\n")
+	def handle_req_deactivate(self, link, content):
+		# Add a deactivation request to the main cache node queue
+		self.queue.put("DEACTIVATE")
 
-    def handle_req_activate(self, link, content):
-        # Add an activation request to the main cache node queue
-        self.queue.put("ACTIVATE")
+		# And trigger the event handler
+		self.hevent.set()
 
-        # And trigger the event handler
-        self.hevent.set()
+		return link.send_response("RDEAC:OK;\r\n")
 
-        return link.send_response("RACTV:OK;\r\n")
+	def handle_req_cache_update(self, link, content):
+		#TODO Update content of database
+		return link.send_response("RCUPD:OK;\r\n")
 
-    def handle_req_deactivate(self, link, content):
-        # Add a deactivation request to the main cache node queue
-        self.queue.put("DEACTIVATE")
+	def handle_req_shutdown(self, link, content):
+		# Add a deactivation request to the main cache node queue
+		self.queue.put("SHUTDOWN")
 
-        # And trigger the event handler
-        self.hevent.set()
+		# And trigger the event handler
+		self.hevent.set()
 
-        return link.send_response("RDEAC:OK;\r\n")
+		return True
+		
+	def handle_req_download(self, link, content):
+		limit = None
+		start_id = None
+		end_id = None
+		unsent_only = False
 
-    def handle_req_shutdown(self, link, content):
-        # Add a deactivation request to the main cache node queue
-        self.queue.put("SHUTDOWN")
+		download_args = content.split(',')
 
-        # And trigger the event handler
-        self.hevent.set()
+		# Parse our argument list
+		if len(download_args) > 0:
+			for arg in download_args:
+				# TODO This part needs to be cleaned up
 
-        return True
-        
-    def handle_req_download(self, link, content):
-        limit = None
-        start_id = None
-        end_id = None
-        unsent_only = False
+				if arg.lower().startswith("limit="):
+					limit = int(arg.split('=')[1])
 
-        download_args = content.split(',')
+				elif arg.lower().startswith("start_id="):
+					# TODO Not yet used!
+					start_id = int(arg.split('=')[1])
 
-        # Parse our argument list
-        if len(download_args) > 0:
-            for arg in download_args:
-                # TODO This part needs to be cleaned up
+				elif arg.lower().startswith("end_id="):
+					# TODO Not yet used!
+					end_id = int(arg.split('=')[1])
 
-                if arg.lower().startswith("limit="):
-                    limit = int(arg.split('=')[1])
+				elif arg.lower().startswith("unsent_only="):
+					# TODO Not yet used!
+					val = arg.split('=')[1]
+					if val == "True":
+						unsent_only = True
+					else:
+						unsent_only = False
 
-                elif arg.lower().startswith("start_id="):
-                    # TODO Not yet used!
-                    start_id = int(arg.split('=')[1])
+		db = DryadDatabase()
+		if db.connect(self.dbname) == False:
+			return False
 
-                elif arg.lower().startswith("end_id="):
-                    # TODO Not yet used!
-                    end_id = int(arg.split('=')[1])
+		if limit == None:
+			limit = 20
 
-                elif arg.lower().startswith("unsent_only="):
-                    # TODO Not yet used!
-                    val = arg.split('=')[1]
-                    if val == "True":
-                        unsent_only = True
-                    else:
-                        unsent_only = False
+		records = db.get_data(limit=limit)
+		while records != None:
+			proc_ids = []
+			resp_data = []
 
-        db = DryadDatabase()
-        if db.connect(self.dbname) == False:
-            return False
+			# Compose our response content
+			for rec in records:
+				proc_ids.append( rec[0] )
+				resp_data.append( {
+					"timestamp" : rec[2],
+					"source" : rec[1],
+					"data" : json.loads(rec[3])
+				} )
+			
+			# Send our response
+			try:
+				resp = json.dumps(resp_data)
+				if link.send_response(resp) == False:
+					self.logger.error("Failed to send response")
+					db.disconnect()
+					return False
+			except:
+				self.logger.error("Failed to send response due to an exception")
+				db.disconnect()
+				return False
+			
+			# Once data is sent successfully, we can mark off the records whose
+			#	IDs we took note of earlier in our database
+			for rec_id in proc_ids:
+				db.set_data_uploaded(rec_id)
 
-        if limit == None:
-            limit = 20
+			# Get a new batch of unsent records
+			records = db.get_data(50)
+			
+			if len(records) < 1:
+				self.logger.info("No more data to send")
+				break
 
-        records = db.get_data(limit=limit)
-        while records != None:
-            proc_ids = []
-            resp_data = []
+			break
 
-            # Compose our response content
-            for rec in records:
-                proc_ids.append( rec[0] )
-                resp_data.append( {
-                    "timestamp" : rec[2],
-                    "source" : rec[1],
-                    "data" : json.loads(rec[3])
-                } )
-            
-            # Send our response
-            try:
-                resp = json.dumps(resp_data)
-                if link.send_response(resp) == False:
-                    self.logger.error("Failed to send response")
-                    db.disconnect()
-                    return False
-            except:
-                self.logger.error("Failed to send response due to an exception")
-                db.disconnect()
-                return False
-            
-            # Once data is sent successfully, we can mark off the records whose
-            #   IDs we took note of earlier in our database
-            for rec_id in proc_ids:
-                db.set_data_uploaded(rec_id)
+		db.disconnect()
 
-            # Get a new batch of unsent records
-            records = db.get_data(50)
-            
-            if len(records) < 1:
-                self.logger.info("No more data to send")
-                break
+		return True
 
-            break
+	def handle_request(self, link, request):
+		# self.logger.info("Message received: {}".format(msg))
+	
+		req_parts = request.split(':', 2)
 
-        db.disconnect()
+		if not len(req_parts) == 2:
+			self.logger.error("Malformed request: {}".format(request))
+			return False
 
-        return True
+		req_hdr = req_parts[0]
+		req_content = req_parts[1].strip(';').strip()
 
-    def handle_request(self, link, request):
-        # self.logger.info("Message received: {}".format(msg))
+		if req_hdr == "QSTAT":
+			# State request
+			if self.handle_req_state(link, req_content) == False:
+				self.logger.error("Failed to handle {} request".format(req_hdr))
+				return False
 
-        req_parts = request.split(':', 2)
+		elif req_hdr == "QACTV":
+			# Activate Cache Node request
+			if self.handle_req_activate(link, req_content) == False:
+				self.logger.error("Failed to handle {} request".format(req_hdr))
+				return False
 
-        if not len(req_parts) == 2:
-            self.logger.error("Malformed request: {}".format(request))
-            return False
+		elif req_hdr == "QDEAC":
+			# Deactivate Cache Node request
+			if self.handle_req_deactivate(link, req_content) == False:
+				self.logger.error("Failed to handle {} request".format(req_hdr))
+				return False
 
-        req_hdr = req_parts[0]
-        req_content = req_parts[1].strip(';').strip()
+		elif req_hdr == "QCUPD":
+			if self.handle_req_cache_update(link, req_content) == False:
+				self.logger.error("Failed to handle {} request".format(req_hdr))
+				return False	
 
-        if req_hdr == "QSTAT":
-            # State request
-            if self.handle_req_state(link, req_content) == False:
-                self.logger.error("Failed to handle {} request".format(req_hdr))
-                return False
+		elif req_hdr == "QPWDN":
+			# Shutdown Program request (undocumented feature)
+			if self.handle_req_shutdown(link, req_content) == False:
+				self.logger.error("Failed to handle {} request".format(req_hdr))
+				return False
 
-        elif req_hdr == "QACTV":
-            # Activate Cache Node request
-            if self.handle_req_activate(link, req_content) == False:
-                self.logger.error("Failed to handle {} request".format(req_hdr))
-                return False
+		elif req_hdr == "QDATA":
+			# Download Data request
+			if self.handle_req_download(link, req_content) == False:
+				self.logger.error("Failed to handle {} request".format(req_hdr))
+				return False
 
-        elif req_hdr == "QDEAC":
-            # Deactivate Cache Node request
-            if self.handle_req_deactivate(link, req_content) == False:
-                self.logger.error("Failed to handle {} request".format(req_hdr))
-                return False
-
-        elif req_hdr == "QPWDN":
-            # Shutdown Program request (undocumented feature)
-            if self.handle_req_shutdown(link, req_content) == False:
-                self.logger.error("Failed to handle {} request".format(req_hdr))
-                return False
-
-        elif req_hdr == "QDATA":
-            # Download Data request
-            if self.handle_req_download(link, req_content) == False:
-                self.logger.error("Failed to handle {} request".format(req_hdr))
-                return False
-
-        return True
+		return True
 
